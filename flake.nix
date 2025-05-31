37063e03b0e0ba5cad6067fb99c3d345b304b372
Nyavielle <nyavielle@gmail.com>
2025-05-31 16:59:08 +0300

    create flake.nix

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "My personal configuration for NixOS";
+
+  inputs = {
+  
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-25.05";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+  };
+
+  outputs = { self, nixpkgs, home-manager, stylix, ... } @inputs:
+  
+  let
+  
+    user = "Nyavielle";
+    hostname = "NyaOS";
+    
+    system = "x86_64-linux";
+    stateVersion = "25.05";
+
+  in {
+  
+    nixosConfigurations = nixpkgs.lib.nixosSystem {
+    
+      system = system;
+
+      specialArgs = { inherit user hostname stateVersion; };
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${user} = ./home-manager/home.nix;
+
+          extraSpecialArgs = { inherit user hostname stateVersion; };
+        }
+        
+      ];
+      
+    };
+    
+  };
+  
+}
